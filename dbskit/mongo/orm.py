de8e5570904638555cc1cad1bf2d3ec26e55f8c6
LavenderUni <uni@LavenderUnideiMac.local>
2016-02-24 12:44:46 +0800

    fix bug

--- dbskit/mongo/orm.py
+++ dbskit/mongo/orm.py
@@ -230,6 +230,9 @@ class Model(dict):
         record = None
         if obj is not None and update:
             updatekeys = {}
+            if '_id' in obj:
+                updatekeys['_id'] = obj['_id']
+                del obj['_id']
             for k, v in obj.__mappings__.iteritems():
                 if v.unique:
                     updatekeys[k] = obj[k]
